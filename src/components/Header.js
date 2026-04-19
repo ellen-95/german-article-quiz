423cccd3e8b44ea30bef4c2c5c6aef70ec97f6e9
Shi <sli20170822@gmail.com>
2026-04-19 18:27:23 +0200

    feat: replace quiz questions with german article dataset

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,8 @@
 function Header() {
   return (
-    <header className='app-header'>
-      <img src='logo512.png' alt='React logo' />
-      <h1>The React Quiz</h1>
+    <header className="app-header">
+      <img src="logo512.png" alt="Quiz logo" />
+      <h1>German Articles Quiz</h1>
     </header>
   );
 }
--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -1,13 +1,13 @@
 function StartScreen({ numQuestions, dispatch }) {
   return (
     <div className="start">
-      <h2>Welcome to the React Quiz!</h2>
-      <h3>{numQuestions} questions to test your React mastery</h3>
+      <h2>Welcome to the German Article Quiz!</h2>
+      <h3>{numQuestions} words to practice der, die, and das</h3>
       <button
         className="btn btn-ui"
         onClick={() => dispatch({ type: "start" })}
       >
-        Let's start
+        Start Quiz
       </button>
     </div>
   );
