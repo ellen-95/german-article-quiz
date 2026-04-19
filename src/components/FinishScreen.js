6b79723ccf87ce8eb422c6c2647a7030fc8253e9
Shi <sli20170822@gmail.com>
2026-04-19 17:09:07 +0200

    chore: initialize project from course quiz app baseline

---
+++ src/components/FinishScreen.js
@@ -0,0 +1,27 @@
+function FinishScreen({ points, maxPossiblePoints, highScore, dispatch }) {
+  const percentage = (points / maxPossiblePoints) * 100;
+  let emoji;
+  if (percentage === 100) emoji = "🏅";
+  if (percentage >= 80 && percentage < 100) emoji = "🎉";
+  if (percentage >= 60 && percentage < 80) emoji = "💪";
+  if (percentage >= 0 && percentage < 60) emoji = "🤔";
+  if (percentage === 0) emoji = "🙈";
+
+  return (
+    <>
+      <p className="result">
+        <span>{emoji}</span>You scored <strong>{points}</strong> out of{" "}
+        {maxPossiblePoints}({Math.ceil(percentage)}%)
+      </p>
+      <p className="highscore">(highscore: {highScore} points)</p>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "restart" })}
+      >
+        Restart Quiz
+      </button>
+    </>
+  );
+}
+
+export default FinishScreen;
